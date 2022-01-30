5f61837281251c6d0eda3e565ee5e56cd7580903
anish jain <helloanishjain@gmail.com>
2022-01-30 22:22:22 +0530

    add header for wallets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
-import { Navbar, Welcome, Footer, Services,Transactions } from "./components";
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
 const App = () => (
   <div className="min-h-screen">
+    <h2 className="text-2xl text-center text-blue-900">Need to install ICONex or Hana  wallet to use this App</h2>
     <div className="gradient-bg-welcome">
+
       <Navbar />
       <Welcome />
     </div>
